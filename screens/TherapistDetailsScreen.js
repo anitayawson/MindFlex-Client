2862865ba70fdb462c81cc2c2a17022a59c065e6
Anita Yawson <anitayawson@gmail.com>
2024-02-22 17:20:55 -0500

    add therapist details screen

--- screens/TherapistDetailsScreen.js
+++ screens/TherapistDetailsScreen.js
@@ -1,13 +1,51 @@
 import { View, Text, Image, TouchableOpacity } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
+import locationIcon from "../assets/icons/map.png";
+import timeIcon from "../assets/icons/time.png";
+import moneyIcon from "../assets/icons/donate.png";
 
 const TherapistDetailsScreen = ({ route }) => {
   const { therapists } = route.params;
 
   return (
-    <View className="">
+    <View className="pt-16 px-6">
       <MaterialIcons name="arrow-back-ios-new" size={24} color="black" />
-      <Text>{therapists.name}</Text>
+      <View className="flex-col items-center">
+        <Image
+          className="rounded-full w-28 h-28"
+          source={{
+            uri: `http://localhost:8080/${therapists.image}`,
+          }}
+        />
+        <Text className="text-lg font-semibold mt-2">{therapists.name}</Text>
+      </View>
+      <View className="mt-4 px-6 flex-row items-center justify-between">
+        <Text className="text-sm">{therapists.role}</Text>
+        <Text>{therapists.rating}(121 reviews)</Text>
+      </View>
+      <View className="flex-col pt-2 pb-4 gap-y-1">
+        <View className="flex-row gap-3 items-center">
+          <Image source={locationIcon} className="w-7 h-7" />
+          <Text className="text-sm">{therapists.location}</Text>
+        </View>
+        <View className="flex-row gap-3 items-center">
+          <Image source={timeIcon} className="w-7 h-7" />
+          <Text className="text-sm">
+            {therapists.experience_years} years of experience
+          </Text>
+        </View>
+        <View className="flex-row gap-3 items-center">
+          <Image source={moneyIcon} className="w-7 h-7" />
+          <Text className="text-sm">${therapists.price}/hr</Text>
+        </View>
+      </View>
+
+      <Text className="font-light leading-5 my-2">{therapists.bio}</Text>
+      <TouchableOpacity className="bg-mindflexGreen flex-row justify-center items-center rounded-xl mt-6 h-14 shadow-xl">
+        <Text className="text-white ml-2 font-semibold">
+          Book An Appointment
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 };
